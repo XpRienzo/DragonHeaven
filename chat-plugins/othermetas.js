40771e069d661387e3179e39018f575381fe698c
Insist <DeathlyPlays@users.noreply.github.com>
2017-05-16 07:55:45 -0500

    Initial Commit
    I basically just did a few travis fixes to awaken Travis

--- chat-plugins/othermetas.js
+++ chat-plugins/othermetas.js
@@ -40,8 +40,7 @@ exports.commands = {
 
 	ce: "crossevolve",
 	crossevo: "crossevolve",
-	crossevolve: function(target, user, room)
-	{
+	crossevolve: function(target, user, room) {
 		if (!this.runBroadcast()) return;
 		if (!target || !target.includes(',')) return this.parse('/help crossevo')
 		let pokes = target.split(",");
@@ -87,7 +86,7 @@ exports.commands = {
 		if (mixedTemplate.weightkg <= 0) {
 			mixedTemplate.weightkg = 0.1;
 		}
-		for (var i in mixedTemplate.baseStats) {
+		for (let i in mixedTemplate.baseStats) {
 			if (mixedTemplate.baseStats[i] < 1 || mixedTemplate.baseStats[i] > 255) {
 				return this.errorReply(`This Cross Evolution cannot happen since a stat goes below 0 or above 255.`);
 			}
@@ -211,7 +210,7 @@ exports.commands = {
 		this.sendReply('|raw|<font size="1">' + Object.keys(details).map(detail => {
 				if (details[detail] === '') return detail;
 				return '<font color="#686868">' + detail + ':</font> ' + details[detail];
-			}).join("&nbsp;|&ThickSpace;") + '</font>');
+		}).join("&nbsp;|&ThickSpace;") + '</font>');
 	},
 	mixandmegahelp: ["/mnm <pokemon> @ <mega stone> - Shows the Mix and Mega evolved Pokemon's type and stats."],
 
@@ -258,7 +257,7 @@ exports.commands = {
 		this.sendReply('|raw|<font size="1">' + Object.keys(details).map(detail => {
 				if (details[detail] === '') return detail;
 				return '<font color="#686868">' + detail + ':</font> ' + details[detail];
-			}).join("&nbsp;|&ThickSpace;") + '</font>');
+		}).join("&nbsp;|&ThickSpace;") + '</font>');
 	},
 	'350cuphelp': ["/350 OR /350cup <pokemon> - Shows the base stats that a Pokemon would have in 350 Cup."],
 
@@ -315,7 +314,7 @@ exports.commands = {
 		this.sendReply('|raw|<font size="1">' + Object.keys(details).map(detail => {
 				if (details[detail] === '') return detail;
 				return '<font color="#686868">' + detail + ':</font> ' + details[detail];
-			}).join("&nbsp;|&ThickSpace;") + '</font>');
+		}).join("&nbsp;|&ThickSpace;") + '</font>');
 	},
 	'tiershifthelp': ["/ts OR /tiershift <pokemon> - Shows the base stats that a Pokemon would have in Tier Shift."],
 
@@ -410,7 +409,7 @@ exports.commands = {
 		this.sendReply('|raw|<font size="1">' + Object.keys(details).map(detail => {
 				if (details[detail] === '') return detail;
 				return '<font color="#686868">' + detail + ':</font> ' + details[detail];
-			}).join("&nbsp;|&ThickSpace;") + '</font>');
+		}).join("&nbsp;|&ThickSpace;") + '</font>');
 	},
 	learnistor: function(target, room, user) {
 		if (!this.runBroadcast()) return;
@@ -428,7 +427,7 @@ exports.commands = {
 		return this.sendReplyBox("In Istor, " + mon.species + ' <font color="red"><u><b>can\'t<b><u></font> learn ' + move.name);
 	},
 	
-	'bnb' : 'badnboosted',
+	'bnb': 'badnboosted',
 	badnboosted : function (target, room, user) {
 		if (!this.runBroadcast()) return;
 		if(!Tools.data.Pokedex[toId(target)]) {
