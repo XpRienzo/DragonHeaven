a3f8c782603745abb0bf9d1e41ad76466b69c691
charizard8888 <omkumr082001@outlook.com>
2018-03-20 22:22:29 +0530

    Update scripts.js

--- mods/eternal/scripts.js
+++ mods/eternal/scripts.js
@@ -2,5 +2,6 @@
 exports.BattleScripts = {
 	init: function () {
 this.modData('Learnsets', 'pansage').learnset.wrathofnature = ['7L1'];
+		this.modData('Learnsets', 'Pansear'.learnset.volcanicexplosion = ['7L1']; 
 	},
 };
