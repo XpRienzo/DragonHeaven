e00707a6ff9a8a7af072f66148c50812bd80cdc6
charizard8888 <omkumr082001@outlook.com>
2018-04-10 11:42:17 +0530

    Update OptiMons movepools

---
+++ mods/opti/scripts.js
@@ -0,0 +1,32 @@
+'use strict';
+
+exports.BattleScripts = {
+init: function () {
+
+this.modData('Learnsets', 'hitmonchan').learnset.backdraft = ['6L1'];
+this.modData('Learnsets', 'hitmonchan').learnset.overclock = ['6L1'];
+this.modData('Learnsets', 'hitmonchan').learnset.overheat = ['6L1'];
+this.modData('Learnsets', 'hitmonchan').learnset.knockoff = ['6L1'];
+
+this.modData('Learnsets', 'hitmonlee').learnset.syncrekick = ['6L1'];
+this.modData('Learnsets', 'hitmonlee').learnset.liquidation = ['6L1'];
+this.modData('Learnsets', 'hitmonlee').learnset.tropkick = ['6L1'];
+this.modData('Learnsets', 'hitmonlee').learnset.knockoff = ['6L1'];
+this.modData('Learnsets', 'hitmonlee').learnset.surf = ['6L1'];
+
+this.modData('Learnsets', 'dragalge').learnset.recover = ['6L1'];
+this.modData('Learnsets', 'dragalge').learnset.poisonmelt = ['6L1'];
+
+this.modData('Learnsets', 'clawitzer').learnset.originpulse = ['6L1'];
+this.modData('Learnsets', 'clawitzer').learnset.dracometeor = ['6L1'];
+
+this.modData('Learnsets', 'ambipom').learnset.stormthrow = ['6L1'];
+this.modData('Learnsets', 'ambipom').learnset.closecombat = ['6L1'];
+this.modData('Learnsets', 'ambipom').learnset.flamewheel = ['6L1'];
+this.modData('Learnsets', 'ambipom').learnset.bulldoze = ['6L1'];
+
+this.modData('Learnsets', 'cinccino').learnset.rapidspin = ['6L1'];
+this.modData('Learnsets', 'cinccino').learnset.tremors = ['6L1'];
+
+},
+};
