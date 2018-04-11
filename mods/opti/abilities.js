c526ea079112840699c76a9929bcf433bcb1e254
charizard8888 <omkumr082001@outlook.com>
2018-04-11 12:24:02 +0530

    Update abilities.js

--- mods/opti/abilities.js
+++ mods/opti/abilities.js
@@ -8,6 +8,7 @@ exports.BattleAbilities = {
 			if (move.type === 'Normal' && !['judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'weatherball'].includes(move.id) && !(move.isZ && move.category !== 'Status')) {
 				move.type = 'Water';
 				move.refrigerateBoosted = true;
+				move.breaksProtect = true;
 			}
 		},
     onAnyAccuracy: function (accuracy, target, source, move) {
