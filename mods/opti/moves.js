e0d9a3805eeedce4f5d8f86db10ec143110775d2
charizard8888 <omkumr082001@outlook.com>
2018-04-11 12:37:09 +0530

    Update moves.js

--- mods/opti/moves.js
+++ mods/opti/moves.js
@@ -18,9 +18,9 @@ exports.BattleMovedex = {
 	"overclock": {
         accuracy: 100,
         basePower: 40,
-		 // basePowerCallback: function (pokemon, target, move) {
-			//return move.basePower + 20 * pokemon.positiveAtkBoosts();
-		//},
+		  basePowerCallback: function (pokemon, target, move) {
+			return move.basePower + 20 * pokemon.positiveAtkBoosts();
+		},
         category: "Physical",
         shortDesc: "This move's Base Power rises by 40 for every stage the Attack stat is boosted. User recovers 50% of the damage dealt.",
         id: "overclock",
