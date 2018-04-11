fd4cdce9887a9a2cdb872ed896898ccb89b7fc2e
charizard8888 <omkumr082001@outlook.com>
2018-04-11 11:47:35 +0530

    Add OptiMons Abilities (Fluid)

---
+++ mods/opti/abilities.js
@@ -0,0 +1,24 @@
+'use strict';
+
+exports.BattleAbilities = {
+"fluid": {
+		shortDesc: "This Pokemon's Normal-type moves become Water type and Water-type moves cannot miss and ignore all protection",
+		onModifyMovePriority: -1,
+		onModifyMove: function (move, pokemon) {
+			if (move.type === 'Normal' && !['judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'weatherball'].includes(move.id) && !(move.isZ && move.category !== 'Status')) {
+				move.type = 'Water';
+				move.refrigerateBoosted = true;
+			}
+		},
+    onModifyAccuracy: function (accuracy, move) {
+			if (typeof accuracy !== 'number') return;
+		  if (move.type === 'Water') {
+      move.accuracy = true,
+      }
+		},
+		id: "fluid",
+		name: "Fluid",
+		rating: 4,
+		num: 174,
+	},
+};
