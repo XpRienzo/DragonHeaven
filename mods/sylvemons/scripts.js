28899d4f7313185dc421d704a273a3c0fe62e30f
charizard8888 <omkumr082001@outlook.com>
2018-04-11 20:30:28 +0530

    Create scripts.js

---
+++ mods/sylvemons/scripts.js
@@ -0,0 +1,28 @@
+'use strict';
+
+exports.BattleScripts = {
+init: function () {
+// Flame Bullet
+	this.modData('Learnsets', 'octillery').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'toucannon').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'celesteela').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'solrock').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'genesect').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'clawitzer').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'charmander').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'charmeleon').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'charizard').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'magy').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'magmar').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'magmortar').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'numel').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'camerupt').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'heatmor').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'fennekin').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'braixen').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'delphox').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'volcanion').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'salazzle').learnset.flamebullet = ['7L1'];
+  this.modData('Learnsets', 'salandit').learnset.flamebullet = ['7L1'];
+	},
+};
