8766fb3e305d6f288ffa150d768f2dd90ec82245
charizard8888 <omkumr082001@outlook.com>
2018-04-12 15:54:20 +0530

    Update items.js

--- mods/sylvemons/items.js
+++ mods/sylvemons/items.js
@@ -247,7 +247,7 @@ exports.BattleItems = {
 		"weatherwarriorscrystal": {
 		shortDesc: "When a weather is active, this peculiar crystal increases the holder's Attack and Special Attack stats by 1 stage each.",
 			onUpdate: function (pokemon) {
-			if (this.isWeather(['sunnyday', 'desolateland', 'hail', 'rainyday', 'primordialsea', 'sandstream']) && pokemon.useItem()) {
+			if (this.isWeather(['sunnyday', 'desolateland', 'hail', 'rainyday', 'primordialsea', 'sandstream', 'shadowsky']) && pokemon.useItem()) {
 				this.boost({atk:1, spa: 1});
 			}
 		},
