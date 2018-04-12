1047e706b164b36dafbae84385bc55e5b6c217ae
charizard8888 <omkumr082001@outlook.com>
2018-04-12 16:22:05 +0530

    Update statuses.js

--- mods/sylvemons/statuses.js
+++ mods/sylvemons/statuses.js
@@ -1,6 +1,40 @@
 'use strict';
 
 exports.BattleStatuses = {
+	hail: {
+		effectType: 'Weather',
+		duration: 5,
+		durationCallback: function (source, effect) {
+			if (source && source.hasItem('icyrock')) {
+				return 8;
+			}
+			return 5;
+		},
+		onStart: function (battle, source, effect) {
+			if (effect && effect.effectType === 'Ability') {
+				if (this.gen <= 5) this.effectData.duration = 0;
+				this.add('-weather', 'Hail', '[from] ability: ' + effect, '[of] ' + source);
+			} else {
+				this.add('-weather', 'Hail');
+			}
+		},
+		onResidualOrder: 1,
+		onResidual: function () {
+			this.add('-weather', 'Hail', '[upkeep]');
+			if (this.isWeather('hail')) this.eachEvent('Weather');
+		},
+		onModifyDef: function (def, pokemon) {
+			if (pokemon.hasType('Water', 'Ice', 'Fire') && this.isWeather('hail')) {
+				return this.modify(def, 1.5);
+			}
+		},
+		onWeather: function (target) {
+			this.damage(target.maxhp / 16);
+		},
+		onEnd: function () {
+			this.add('-weather', 'none');
+		},
+	},
 shadowsky: {
 		effectType: 'Weather',
 		duration: 5,
