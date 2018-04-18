ffbca7f3c9d235bd1817a342fe5d9288517292ac
Spandamn <spandan.punwatkar@gmail.com>
2018-04-18 19:32:47 +0530

    Torrid Sand in Eevee'd

--- mods/eeveed/abilities.js
+++ mods/eeveed/abilities.js
@@ -1,47 +1,52 @@
 'use strict';
 
 exports.BattleAbilities = {
-	/*"torridsand": {
-                shortDesc: "If Psychic Terrain is active, this Pokemon's Speed is doubled.",
-		            onEffectiveness: function (typeMod, type, move, target) {
-						if (move.type === 'Ground' && target.type === 'Water') {
-                        return 1;
-						}
-						},
-		            id: "torridsand",
-		            name: "Torrid Sand",
-	      },*/
-		"electrojection": {
+	"torridsand": {
+		shortDesc: "If Psychic Terrain is active, this Pokemon's Speed is doubled.",
+		onModifyMove: function(move) {
+			if (move.type !== "Ground") return;
+			move.onEffectiveness: function(typeMod, type) {
+				if (type === 'Water') return 1;
+			},
+		},
+		id: "torridsand",
+		name: "Torrid Sand",
+	},
+	"electrojection": {
 		shortDesc: "This Pokemon can paralyze other Pokemon regardless of their typing.",
 		// Implemented in sim/pokemon.js:Pokemon#setStatus
 		id: "electrojection",
 		name: "Electrojection",
 	},
-        "miracledash": {
-                shortDesc: "If Psychic Terrain is active, this Pokemon's Speed is doubled.",
-		            onModifySpe: function (spe) {
-			                  if (this.isTerrain('psychicterrain')) {
-				                        return this.chainModify(2);
-			                  }
-		            },
-		            id: "miracledash",
-		            name: "Miracle Dash",
-	      },
+	"miracledash": {
+		shortDesc: "If Psychic Terrain is active, this Pokemon's Speed is doubled.",
+		onModifySpe: function(spe) {
+			if (this.isTerrain('psychicterrain')) {
+				return this.chainModify(2);
+			}
+		},
+		id: "miracledash",
+		name: "Miracle Dash",
+	},
 	"bravery": {
 		shortDesc: "This Pokemon's Attack is raised 1 stage if hit by a Dark move; Dark immunity.",
 		onTryHitPriority: 1,
-		onTryHit: function (target, source, move) {
+		onTryHit: function(target, source, move) {
 			if (target !== source && move.type === 'Dark') {
-				if (!this.boost({atk:1})) {
+				if (!this.boost({
+						atk: 1
+					})) {
 					this.add('-immune', target, '[msg]', '[from] ability: Bravery');
 				}
 				return null;
 			}
 		},
-		onAllyTryHitSide: function (target, source, move) {
+		onAllyTryHitSide: function(target, source, move) {
 			if (target === this.effectData.target || target.side !== source.side) return;
 			if (move.type === 'Dark') {
-				this.boost({atk:1}, this.effectData.target);
+				this.boost({
+					atk: 1
+				}, this.effectData.target);
 			}
 		},
 		id: "bravery",
@@ -50,9 +55,11 @@ exports.BattleAbilities = {
 	},
 	"hiddensoul": {
 		shortDesc: "This Pokemon's Special Defense is raised by 1 stage after it is damaged by a move.",
-		onAfterDamage: function (damage, target, source, effect) {
+		onAfterDamage: function(damage, target, source, effect) {
 			if (effect && effect.effectType === 'Move' && effect.id !== 'confused') {
-				this.boost({spd:1});
+				this.boost({
+					spd: 1
+				});
 			}
 		},
 		id: "hiddensoul",
@@ -62,18 +69,22 @@ exports.BattleAbilities = {
 	"frostborn": {
 		shortDesc: "This Pokemon's Attack is raised 1 stage if hit by an Ice move; Ice immunity.",
 		onTryHitPriority: 1,
-		onTryHit: function (target, source, move) {
+		onTryHit: function(target, source, move) {
 			if (target !== source && move.type === 'Ice') {
-				if (!this.boost({atk:1})) {
+				if (!this.boost({
+						atk: 1
+					})) {
 					this.add('-immune', target, '[msg]', '[from] ability: Frostborn');
 				}
 				return null;
 			}
 		},
-		onAllyTryHitSide: function (target, source, move) {
+		onAllyTryHitSide: function(target, source, move) {
 			if (target === this.effectData.target || target.side !== source.side) return;
 			if (move.type === 'Ice') {
-				this.boost({atk:1}, this.effectData.target);
+				this.boost({
+					atk: 1
+				}, this.effectData.target);
 			}
 		},
 		id: "frostborn",
@@ -89,15 +100,15 @@ exports.BattleAbilities = {
 		name: "Lucky",
 		rating: 3.5,
 	},
-		"unbreakable": {
+	"unbreakable": {
 		shortDesc: "This Pokemon is immune to punch-based moves.",
-		onTryHit: function (target, source, move) {
+		onTryHit: function(target, source, move) {
 			if (move.flags['punch']) {
 				this.add('-immune', target, '[msg]', '[from] ability: Unbreakable');
 				return null;
 			}
 		},
-		onAllyTryHitSide: function (target, source, move) {
+		onAllyTryHitSide: function(target, source, move) {
 			if (move.flags['punch']) {
 				this.add('-immune', this.effectData.target, '[msg]', '[from] ability: Unbreakable');
 			}
@@ -108,36 +119,36 @@ exports.BattleAbilities = {
 	},
 	"lucky": {
 		shortDesc: "This mons attacks always crit unless Lucky Shield, shell armour or battle armour are active.",
-		onModifyCritRatio: function (critRatio) {
+		onModifyCritRatio: function(critRatio) {
 			return critRatio + 6;
 		},
 		id: "lucky",
 		name: "Lucky",
 	},
-"quickclaws": {
+	"quickclaws": {
 		shortDesc: "Any attack with 60 BP or less gets a +1 to priority",
-		onModifyPriority: function (priority, pokemon, target, move) {
-			if (move.basePower <=60 ) return priority + 1;
+		onModifyPriority: function(priority, pokemon, target, move) {
+			if (move.basePower <= 60) return priority + 1;
 		},
 		id: "quickclaws",
 		name: "Quick Claws",
 	},
-		"reaperslice": { /* Make the 1.2 somehow 1.3 + Add the infiltrating effect*/
+	"reaperslice": { /* Make the 1.2 somehow 1.3 + Add the infiltrating effect*/
 		desc: "Ghost type moves can bypass Subsitutes, they have also x1.3 power",
 		shortDesc: "Ghost type moves can bypass Subsitutes, they have also x1.3 power",
 		onModifyMovePriority: -1,
-		onModifyMove: function (move, pokemon) {
+		onModifyMove: function(move, pokemon) {
 			if (move.type === 'Normal' && !['judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'weatherball'].includes(move.id) && !(move.isZ && move.category !== 'Status')) {
 				move.type = 'Ghost';
 				move.sliceBoosted = true;
 			}
 		},
 		onBasePowerPriority: 8,
-		onBasePower: function (basePower, pokemon, target, move) {
+		onBasePower: function(basePower, pokemon, target, move) {
 			if (move.sliceBoosted) return this.chainModify([0x1333, 0x1000]);
 		},
 		id: "reaperslice",
 		name: "Reaper Slice",
 		rating: 4,
 	},
-};        
+};
