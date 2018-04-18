88c9f86d8b268ce87aeb9450dd400d5252bc3d38
charizard8888 <omkumr082001@outlook.com>
2018-04-18 13:56:15 +0530

    Update abilities.js

--- mods/eeveed/abilities.js
+++ mods/eeveed/abilities.js
@@ -101,7 +101,9 @@ exports.BattleAbilities = {
 "quickclaws": {
 		shortDesc: "Any attack with 60 BP or less gets a +1 to priority",
 		onModifyMove: function (basePower, pokemon, priority, move) {
-			if (move.basePower <= 60) return priority + 1;
+			if (move.basePower <= 60) {
+				move.priority + 1;
+			}
 		},
 		id: "quickclaws",
 		name: "Quick Claws",
