d841df391bfdd1a5a4de199a49c022bab4f21876
charizard8888 <omkumr082001@outlook.com>
2018-04-19 20:18:30 +0530

    Update moves.js

--- mods/sylvemons/moves.js
+++ mods/sylvemons/moves.js
@@ -66,9 +66,9 @@ exports.BattleMovedex = {
 					this.debug('misty terrain weaken');
 					return this.chainModify(0.5);
 				}
-				if (move.type === 'Fairy' && attacker.isGrounded() && !attacker.isSemiInvulnerable()) {
+				/*if (move.type === 'Fairy' && attacker.isGrounded() && !attacker.isSemiInvulnerable()) {
 					return this.chainModify(1.5);
-				}
+				}*/
 			},
 			onStart: function (battle, source, effect) {
 				if (effect && effect.effectType === 'Ability') {
