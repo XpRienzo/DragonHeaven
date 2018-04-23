d78546637f33acff9e034f84f75bfaa73c528cdc
charizard8888 <omkumr082001@outlook.com>
2018-04-23 11:53:37 +0530

    Add SylveMons moves distribution (Kudos to Origin0)

--- mods/sylvemons/scripts.js
+++ mods/sylvemons/scripts.js
@@ -24,5 +24,64 @@ init: function () {
   this.modData('Learnsets', 'volcanion').learnset.flamebullet = ['7L1'];
   this.modData('Learnsets', 'salazzle').learnset.flamebullet = ['7L1'];
   this.modData('Learnsets', 'salandit').learnset.flamebullet = ['7L1'];
+	
+	// Fairy Charge
+   this.modData('Learnsets', 'granbull').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'tapubulu').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'slurpuff').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'marill').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'azumarill').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'mawile').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'xerneas').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'togekiss').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'gallade').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'cleffa').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'clefairy').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'clefable').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'igglybuff').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'jigglypuff').learnset.fairycharge = ['7L1'];
+   this.modData('Learnsets', 'wigglytuff').learnset.fairycharge = ['7L1'];
+
+// Recrystalize
+   this.modData('Learnsets', 'diancie').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'carbink').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'onix').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'steelix').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'snorunt').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'glalie').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'froslass').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'bergmite').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'avalugg').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'roggenrola').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'boldore').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'gigalith').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'sableye').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'dialga').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'vanillite').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'vanillish').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'vanilluxe').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'amaura').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'auroros').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'sandshrewalola').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'sandslashalola').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'glaceon').learnset.recrystalize = ['7L1'];
+   this.modData('Learnsets', 'regice').learnset.recrystalize = ['7L1'];
+
+// Stalwart Sword
+   this.modData('Learnsets', 'samurott').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'cobalion').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'terrakion').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'virizion').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'keldeo').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'kartana').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'honedge').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'doublade').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'aegislash').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'gallade').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'escavalier').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'pawniard').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'bisharp').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'scyther').learnset.stalwartsword = ['7L1'];
+   this.modData('Learnsets', 'scizor').learnset.stalwartsword = ['7L1'];
 	},
 };
