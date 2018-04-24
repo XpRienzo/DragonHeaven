1b920bf24aa8b43e8b4d4cbe03792806f7b74154
charizard8888 <omkumr082001@outlook.com>
2018-04-24 13:43:22 +0530

    Update abilities.js

--- mods/fe/abilities.js
+++ mods/fe/abilities.js
@@ -4358,8 +4358,8 @@ exports.BattleAbilities = {
 	},
 	"magicfat": {
 		shortDesc: "Immune to Fire and Ice type moves as long as it holds an item.",
-		onImmunity: function (type, pokemon) {
-			if (pokemon.item && type === 'Fire' || type === 'Ice') return false;
+		onImmunity: function (type, target) {
+			if (target.item && type === 'Fire' || type === 'Ice') return false;
 		},
 		id: "magicfat",
 		name: "Magic Fat",
