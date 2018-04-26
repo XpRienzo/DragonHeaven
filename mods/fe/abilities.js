30f55bce3b0b9f4f2082987d967e928705e55300
charizard8888 <omkumr082001@outlook.com>
2018-04-26 16:46:34 +0530

    Update abilities.js

--- mods/fe/abilities.js
+++ mods/fe/abilities.js
@@ -5231,12 +5231,12 @@ exports.BattleAbilities = {
 	},
 	"confiscation": {
 		shortDesc: "Any Pokemon that tries to switch out on it will lose its item as a result.",
-		/*onFoeSwitchOut: function (pokemon) {
+		onFoeSwitchOut: function (pokemon) {
 			for (const target of pokemon.side.foe.active) {
 			if (!target || target.fainted) continue;
 			this.add('-enditem', target, '[from] move: Knock Off', '[of] ' + pokemon);
 			}
-		},*/
+		},
 		onSwitchOut: function (pokemon) {
 			this.add('-enditem', pokemon, '[from] move: Knock Off', '[of] ' + pokemon);
 		},
