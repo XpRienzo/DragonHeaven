668e714bcb7fa6723d079b82094ddf3af0a0fbff
Ludicrousity <nathangtcooper@gmail.com>
2018-04-26 15:59:02 +0100

    Update random-teams.js

--- mods/dhssb/random-teams.js
+++ mods/dhssb/random-teams.js
@@ -38,7 +38,7 @@ class RandomSeasonalTeams extends RandomTeams {
 				nature:'Jolly',
 			},
 			'Pika Xreme' : {
-				species: 'Raichu', ability: 'Xreme Bulk', item: 'Leftovers', gender: 'M', shiny:true,
+				species: 'Raichu', ability: 'Warpstar Crusader', item: 'Leftovers', gender: 'M', shiny:true,
 				moves: ['volttackle', 'woodhammer', 'icehammer'],
 				signatureMove: "Healing Area",evs: {hp: 252, atk:252, spd: 252, def: 252, spe: 252},
 				nature:'Adamant',
