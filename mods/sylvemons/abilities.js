b7a6f47c1b7265084563da6aa7a1dfbba74bb83f
charizard8888 <omkumr082001@outlook.com>
2018-04-28 11:46:46 +0530

    Update abilities.js

--- mods/sylvemons/abilities.js
+++ mods/sylvemons/abilities.js
@@ -132,7 +132,8 @@ Obstinacy	User gains a boost in it's moves the lower it's HP gets. Formula:  (1.
 	},
 	housekeeping: {
 		shortDesc: "Removes hazards upon switch-in.",
-	onSwitch: function (pokemon, target, source) {
+		onSwitchInPriority: 1,
+		onSwitchIn: function (pokemon, target, source) {
 						this.removePseudoWeather('trickroom');
 			        this.removePseudoWeather('magicroom');
 			 		this.removePseudoWeather('wonderroom');
