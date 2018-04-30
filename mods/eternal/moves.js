8f1c71cec9e164d16e03a8728c7e5513db9da7c1
charizard8888 <omkumr082001@outlook.com>
2018-04-30 20:53:16 +0530

    Update moves.js

--- mods/eternal/moves.js
+++ mods/eternal/moves.js
@@ -1268,7 +1268,7 @@ Z-Move Effect: Does a 25BP Z-Move for all 8 attacks. (E.g, Hydro Vortex -> Gigav
 		pp: 10,
 		priority: 0,
 		flags: {protect: 1, mirror: 1},
-		onSourceFaint: function (pokemon) {
+		onHit: function (pokemon) {
 			if (this.activeMove && this.activeMove.id === 'skillswap') return;
 			let target = pokemon.side.foe.active[pokemon.side.foe.active.length - 1 - pokemon.position];
 			if (target) {
