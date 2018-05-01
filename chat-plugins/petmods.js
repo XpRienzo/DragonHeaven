94135c653c1a3ebbdda5580f3abb064f7714d401
charizard8888 <omkumr082001@outlook.com>
2018-05-01 13:45:59 +0530

    Update petmods.js

--- chat-plugins/petmods.js
+++ chat-plugins/petmods.js
@@ -226,12 +226,12 @@ typeopthelp: ["/eternalmons - Shows the list of Pokemon in Type Optimisation Pok
 		});
 		this.sendReplyBox(`${buf}</div>`);
 	},
-	femoves: function (target, room, user) {
+	sylvemoves: function (target, room, user) {
 		if (!this.runBroadcast()) return;
-		let buf = `<div class=infobox-limited><center><h2>List Of Coded Fusion Evolution Moves</h2></center>`;
-		let feDex = require('../mods/fe/moves.js').BattleMovedex ;
-		if (!feDex) return this.errorReply("Error Fetching FE Data.");
-		Object.values(feDex).forEach(move => {
+		let buf = `<div class=infobox-limited><center><h2>List Of Coded Fusion Moves</h2></center>`;
+		let sylveDex = require('../mods/fe/moves.js').BattleMovedex;
+		if (!sylveDex) return this.errorReply("Error Fetching FE Data.");
+		Object.values(sylveDex).forEach(move => {
 			buf += `<button name="send" value="/dt ${move.id}, FE" style="background:none;border:none;">${move.id}</button><br>`;
 		});
 		this.sendReplyBox(`${buf}</div>`);
