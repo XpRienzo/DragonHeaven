9cc60e2495d64fc42f08f6b866b6755200693024
charizard8888 <omkumr082001@outlook.com>
2018-05-05 11:36:38 +0530

    Create formats-data.js

---
+++ mods/eternal/formats-data.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateFormatsData}} */
+let BattleFormatsData = {
+
+floette: {
+		randomBattleMoves: ["moonblast", "toxic", "wish", "psychic", "aromatherapy", "protect", "calmmind"],
+		tier: "NFE",
+	},
+	floetteeternal: {
+		randomBattleMoves: ["lightofruin", "psychic", "hiddenpowerfire", "hiddenpowerground", "moonblast"],
+		randomDoubleBattleMoves: ["lightofruin", "dazzlinggleam", "wish", "psychic", "aromatherapy", "protect", "calmmind"],
+		isUnreleased: false,
+		tier: "OU",
+	},
+ };
