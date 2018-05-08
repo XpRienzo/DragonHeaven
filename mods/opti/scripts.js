f8bdafb430636d04fbbccbc16a51405d2fbf71f0
charizard8888 <omkumr082001@outlook.com>
2018-05-08 11:55:05 +0530

    Update scripts.js

--- mods/opti/scripts.js
+++ mods/opti/scripts.js
@@ -1,43 +1,25 @@
 'use strict';
 
 exports.BattleScripts = {
-pokemon: { 
-setStatus(status, source = null, sourceEffect = null, ignoreImmunities = false) {
-if (!ignoreImmunities && status.id && !(source && source.hasAbility('electrojection') && ['par'].includes(status.id))) {
-			// the game currently never ignores immunities
-			if (!this.runStatusImmunity(status.id === 'par' ? 'par' : status.id)) {
-				this.battle.debug('immune to status');
-				if (sourceEffect && sourceEffect.status) this.battle.add('-immune', this, '[msg]');
-				return false;
-			}
-		}
-},
 init: function () {
-
 this.modData('Learnsets', 'hitmonchan').learnset.backdraft = ['6L1'];
 this.modData('Learnsets', 'hitmonchan').learnset.overclock = ['6L1'];
 this.modData('Learnsets', 'hitmonchan').learnset.overheat = ['6L1'];
 this.modData('Learnsets', 'hitmonchan').learnset.knockoff = ['6L1'];
-
 this.modData('Learnsets', 'hitmonlee').learnset.syncrekick = ['6L1'];
 this.modData('Learnsets', 'hitmonlee').learnset.liquidation = ['6L1'];
 this.modData('Learnsets', 'hitmonlee').learnset.tropkick = ['6L1'];
 this.modData('Learnsets', 'hitmonlee').learnset.knockoff = ['6L1'];
 this.modData('Learnsets', 'hitmonlee').learnset.surf = ['6L1'];
-
 this.modData('Learnsets', 'dragalge').learnset.recover = ['6L1'];
 this.modData('Learnsets', 'dragalge').learnset.poisonmelt = ['6L1'];
-
 this.modData('Learnsets', 'clawitzer').learnset.originpulse = ['6L1'];
 this.modData('Learnsets', 'clawitzer').learnset.dracometeor = ['6L1'];
-
 this.modData('Learnsets', 'ambipom').learnset.stormthrow = ['6L1'];
 this.modData('Learnsets', 'ambipom').learnset.closecombat = ['6L1'];
 this.modData('Learnsets', 'ambipom').learnset.flamewheel = ['6L1'];
 this.modData('Learnsets', 'ambipom').learnset.bulldoze = ['6L1'];
-
 this.modData('Learnsets', 'cinccino').learnset.rapidspin = ['6L1'];
 this.modData('Learnsets', 'cinccino').learnset.tremors = ['6L1'];
-
 },
 };
