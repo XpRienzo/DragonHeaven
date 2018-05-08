11ae569ee49fdcab24cf54b41ef1d1a59bce3773
charizard8888 <omkumr082001@outlook.com>
2018-05-08 16:02:33 +0530

    Update moves.js

--- mods/aggressionpassion/moves.js
+++ mods/aggressionpassion/moves.js
@@ -25,6 +25,83 @@ let BattleMovedex = {
 		zMoveEffect: 'clearnegativeboost',
 		contestType: "Tough",
 	},
+	"agility": {
+		num: 97,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "Raises the user's Speed by 2 stages.",
+		shortDesc: "Raises the user's Speed by 2.",
+		id: "agility",
+		isViable: true,
+		name: "Agility",
+		pp: 30,
+		priority: 0,
+		flags: {snatch: 1},
+		selfBoost: {
+		boosts: {
+			spe: 2,
+		},
+		},
+		secondary: false,
+		target: "self",
+		type: "Psychic",
+		zMoveEffect: 'clearnegativeboost',
+		contestType: "Cool",
+	},
+	"amnesia": {
+		num: 133,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "Raises the user's Special Defense by 2 stages.",
+		shortDesc: "Raises the user's Sp. Def by 2.",
+		id: "amnesia",
+		name: "Amnesia",
+		pp: 20,
+		priority: 0,
+		flags: {snatch: 1},
+		selfBoost: {
+		boosts: {
+			spd: 2,
+		},
+		},
+		secondary: false,
+		target: "self",
+		type: "Psychic",
+		zMoveEffect: 'clearnegativeboost',
+		contestType: "Cute",
+	},
+	"aquaring": {
+		num: 392,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "The user has 1/16 of its maximum HP, rounded down, restored at the end of each turn while it remains active. If the user uses Baton Pass, the replacement will receive the healing effect.",
+		shortDesc: "User recovers 1/16 max HP per turn.",
+		id: "aquaring",
+		name: "Aqua Ring",
+		pp: 20,
+		priority: 0,
+		flags: {snatch: 1},
+		self: {
+			volatileStatus: 'aquaring',
+		},
+		effect: {
+			onStart: function (pokemon) {
+				this.add('-start', pokemon, 'Aqua Ring');
+			},
+			onResidualOrder: 6,
+			onResidual: function (pokemon) {
+				this.heal(pokemon.maxhp / 16);
+			},
+		},
+		secondary: false,
+		target: "self",
+		type: "Water",
+		zMoveBoost: {def: 1},
+		contestType: "Beautiful",
+	},
 };
-
+	//TODO: Acupressure, Ally Switch
 exports.BattleMovedex = BattleMovedex;
