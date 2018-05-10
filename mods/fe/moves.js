d7aeae9eb716f0104e3cc77f18ea42d575b9b3a7
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2018-05-10 16:56:30 +0530

    Update moves.js (#779)
    Compatibility with the Beautiful Obliteration Weapon ability

--- mods/fe/moves.js
+++ mods/fe/moves.js
@@ -6272,5 +6272,58 @@ exports.BattleMovedex = {
 		zMovePower: 200,
 		contestType: "Tough",
 	},
-
+	"beautifulterrain": {
+		num: 0,
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		desc: "Simply summons Beautiful Terrain",
+		id: "beautifulterrain",
+		name: "Beautiful Terrain",
+		pp: 10,
+		priority: 0,
+		flags: {nonsky: 1},
+		terrain: 'beautifulterrain',
+		effect: {
+			duration: 5,
+			durationCallback: function (source, effect) {
+				if (source && source.hasItem('terrainextender')) {
+					return 8;
+				}
+				return 5;
+			},
+			onBasePower: function (basePower, attacker, defender, move) {
+				if ((move.type === 'Fire' || move.type === 'Fairy') && attacker.isGrounded()) {
+					this.debug('beautiful terrain boost');
+					return this.chainModify(1.5);
+				}
+			},
+			onStart: function (battle, source, effect) {
+				if (effect && effect.effectType === 'Ability') {
+					this.add('-fieldstart', 'move: Beautiful Terrain', '[from] ability: ' + effect, '[of] ' + source);
+				} else {
+					this.add('-fieldstart', 'move: Beautiful Terrain');
+				}
+			},
+			onResidualOrder: 5,
+			onResidualSubOrder: 3,
+			onResidual: function () {
+				this.eachEvent('Terrain');
+			},
+			onTerrain: function (pokemon) {
+				if (pokemon.isGrounded() && !pokemon.isSemiInvulnerable()) {
+					this.debug('Pokemon is grounded, burning through Beautiful Terrain.');
+			                pokemon.trySetStatus('brn', pokemon);
+				}
+			},
+			onEnd: function () {
+				this.eachEvent('Terrain');
+				this.add('-fieldend', 'move: Beautiful Terrain');
+			},
+		},
+		secondary: false,
+		target: "all",
+		type: "Fire",
+		zMoveBoost: {spd: 1},
+	},
 };
