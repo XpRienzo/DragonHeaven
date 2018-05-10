f23491e17f1730a29b8d45b6e44e932bd60a8fab
charizard8888 <omkumr082001@outlook.com>
2018-05-10 13:50:29 +0530

    Update statuses.js

--- mods/fe/statuses.js
+++ mods/fe/statuses.js
@@ -31,6 +31,7 @@ solarsnow: {
 				this.add('-weather', 'SolarSnow', '[from] ability: ' + effect, '[of] ' + source);
 			} else {
 				this.add('-weather', 'SolarSnow');
+				this.add('SolarSnow');
 			}
 		},
 		onResidualOrder: 1,
@@ -39,7 +40,7 @@ solarsnow: {
 			if (this.isWeather('solarsnow')) this.eachEvent('Weather');
 		},
 		onWeather: function (target) {
-         if (!(target.hasType('Grass') || target.hasType('Fire') || target.hasType('Ice'))){
+         if (!target.hasType('Grass') || !target.hasType('Fire') || !target.hasType('Ice'))){
 			  this.damage(target.maxhp / 16);
                         }
 		},
