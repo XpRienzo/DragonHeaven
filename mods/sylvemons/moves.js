b55f184660f3462f9572b46f28033e70a6ea6525
charizard8888 <omkumr082001@outlook.com>
2018-05-11 21:07:44 +0530

    Update moves.js

--- mods/sylvemons/moves.js
+++ mods/sylvemons/moves.js
@@ -24,6 +24,46 @@ sound: Has no effect on Pokemon with the Ability Soundproof.
 */
 'use strict';
 exports.BattleMovedex = {
+		"inverseroom": {
+		accuracy: true,
+		basePower: 0,
+		category: "Status",
+		shortDesc: "For 5 turns, all Pokémon on the field are resistant to normally super-effective types and weak to normally not-very-effective or ineffective types (as in Inverse Battles) ",
+		id: "inverseroom",
+		name: "Inverse Room",
+		pp: 8,
+		priority: 0,
+		flags: {mirror: 1},
+		pseudoWeather: 'inverseroom',
+		effect: {
+			duration: 5,
+			durationCallback: function(source, effect) {
+				if (source && source.hasAbility('persistent')) {
+					return 7;
+				}
+				return 5;
+			},
+			onStart: function(target, source) {
+				this.add('-fieldstart', 'move: Inverse Room', '[of] ' + source);
+			},
+			onEffectiveness: function(typeMod, target, type, move) {
+				if (move && !this.getImmunity(move, type)) return 1;
+				return -typeMod;
+			},
+			onResidualOrder: 23,
+			onEnd: function() {
+				this.add('-fieldend', 'move: Inverse Room');
+			},
+		},
+		onPrepareHit: function(target, source, move) {
+			this.attrLastMove('[still]');
+			this.add('-anim', target, "Sunny Day", source);
+		},
+		secondary: false,
+		target: "all",
+		type: "Psychic",
+		zMoveBoost: {acc: 1},
+	},
 	"shadowcharge": {
 		accuracy: 95,
 		basePower: 85,
