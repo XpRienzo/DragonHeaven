ef0b8792cf08849d23af1445275c6eb625f95b3a
charizard8888 <omkumr082001@outlook.com>
2018-05-14 12:07:19 +0530

    Update petmods.js

--- chat-plugins/petmods.js
+++ chat-plugins/petmods.js
@@ -434,7 +434,7 @@ evgutter: function (target, room, user) {
 		let eternalDex = require('../mods/eternal/moves.js').BattleMovedex;
 		if (!eternalDex) return this.errorReply("Error Fetching Eternal Data.");
 		Object.values(eternalDex).forEach(move => {
-			if (move.category !== 'Status' && move.zMovePower) {
+			if (move.category !== 'Status' && !move.zMovePower) {
 			buf += `<button name="send" value="/dt ${move.id}, Eternal" style="background:none;border:none;">${move.id}</button><br>`;
 			}
 		});
