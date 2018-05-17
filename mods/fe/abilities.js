00664c924aa96632ffa876e9f3e6996ac7ece4be
charizard8888 <omkumr082001@outlook.com>
2018-05-17 14:06:00 +0530

    Update abilities.js

--- mods/fe/abilities.js
+++ mods/fe/abilities.js
@@ -8923,7 +8923,13 @@ exports.BattleAbilities = {
 	},
 	"resurrection": {
 		shortDesc: "When this Pokémon gets KOed for the first time, it gains 50% of its original HP and changes to Reborn form.",
-		onDamage: function (damage, target, source, move) {
+		onDamage: function (damage, target, source, effect) {
+			if (target.hp === target.maxhp && damage >= target.hp && effect && effect.effectType === 'Move') {
+				this.add('-ability', target, 'Resurrection');
+				return target.hp - 1;
+			}
+		},
+		onAfterDamage: function (damage, target, source, move) {
 			if (target.baseTemplate.species === 'Miminja' && source !== target && move && move.effectType === 'Move' && !move.isFutureMove) {
 				this.add('-activate', target, 'ability: Resurrection');
 				this.add('-formechange', target, 'Miminja-Reborn', '[msg]');
