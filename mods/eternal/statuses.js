ce3ee61599b1c5dce32d254a38f14047008d3bb7
charizard8888 <omkumr082001@outlook.com>
2018-05-19 12:23:32 +0530

    Update statuses.js

--- mods/eternal/statuses.js
+++ mods/eternal/statuses.js
@@ -16,10 +16,20 @@ despoilingvines: {
 			this.add('-activate', pokemon, 'move: ' + this.effectData.sourceEffect, '[of] ' + source);
 		},
 		onResidualOrder: 11,
-		onResidual: function (source, pokemon) {
+		onResidual: function (pokemon) {
+			if (this.effectData.source && (!this.effectData.source.isActive || this.effectData.source.hp <= 0 || !this.effectData.source.activeTurns)) {
+				delete pokemon.volatiles['despoilingvines'];
+				return;
+			}
+			if (this.effectData.source.hasItem('bindingband')) {
+				this.damage(pokemon.maxhp / 6);
+			} else {
 				this.damage(pokemon.maxhp / 8);
-				this.heal(source.maxhp / 8);
+			}
 		},
+	  onAfterDamage: function (source) {
+		  this.heal(source.maxhp / 8);
+	  },
 		onEnd: function (pokemon) {
 			this.add('-end', pokemon, this.effectData.sourceEffect, '[despoilingvines]');
 		},
