d2bd89ba9d59022151a6476b00c24d3d733a7546
charizard8888 <omkumr082001@outlook.com>
2018-05-22 15:40:20 +0530

    Update and rename scripts (Testing something) to scripts.js

---
+++ mods/fe/scripts.js
@@ -0,0 +1,9 @@
+/*'use strict';
+
+const CHOOSABLE_TARGETS = new Set(['normal', 'any', 'adjacentAlly', 'adjacentAllyOrSelf', 'adjacentFoe']);*/
+
+/**@type {BattleScriptsData} */
+/*let BattleScripts = {
+
+};
+exports.BattleScripts = BattleScripts;*/
