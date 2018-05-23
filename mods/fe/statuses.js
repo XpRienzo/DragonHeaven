48c62f16061aeb277a214a5064ba92398413b0d7
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2018-05-23 08:39:57 -0400

    Update statuses.js

--- mods/fe/statuses.js
+++ mods/fe/statuses.js
@@ -2,7 +2,79 @@
 
 /**@type {{[k: string]: EffectData}} */
 let BattleStatuses = {
-
+	//Therapeutic and Shut Up and Jam allow movement under status at all times (excluding Sleep for the former).
+	par: {
+		name: 'par',
+		id: 'par',
+		num: 0,
+		effectType: 'Status',
+		onStart: function (target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'par', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'par');
+			}
+		},
+		onModifySpe: function (spe, pokemon) {
+			if (!pokemon.hasAbility('quickfeet')) {
+				return this.chainModify(0.5);
+			}
+		},
+		onBeforeMovePriority: 1,
+		onBeforeMove: function (pokemon) {
+			if (this.randomChance(1, 4) && !pokemon.hasAbility('therapeutic') && !pokemon.hasAbility('shutupandjam')) {
+				this.add('cant', pokemon, 'par');
+				return false;
+			}
+		},
+	},
+	frz: {
+		name: 'frz',
+		id: 'frz',
+		num: 0,
+		effectType: 'Status',
+		onStart: function (target, source, sourceEffect) {
+			if (sourceEffect && sourceEffect.effectType === 'Ability') {
+				this.add('-status', target, 'frz', '[from] ability: ' + sourceEffect.name, '[of] ' + source);
+			} else {
+				this.add('-status', target, 'frz');
+			}
+			if (target.template.species === 'Shaymin-Sky' && target.baseTemplate.baseSpecies === 'Shaymin') {
+				let template = this.getTemplate('Shaymin');
+				target.formeChange(template);
+				target.baseTemplate = template;
+				target.setAbility(template.abilities['0'], null, true);
+				target.baseAbility = target.ability;
+				target.details = template.species + (target.level === 100 ? '' : ', L' + target.level) + (target.gender === '' ? '' : ', ' + target.gender) + (target.set.shiny ? ', shiny' : '');
+				this.add('detailschange', target, target.details);
+				this.add('-formechange', target, 'Shaymin', '[msg]');
+			}
+		},
+		onBeforeMovePriority: 10,
+		onBeforeMove: function (pokemon, target, move) {
+			if (move.flags['defrost']) return;
+			if (this.randomChance(1, 5)) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.add('cant', pokemon, 'frz');
+			if (pokemon.hasAbility('therapeutic') || pokemon.hasAbility('shutupandjam')){
+				return;
+			}
+			return false;
+		},
+		onModifyMove: function (move, pokemon) {
+			if (move.flags['defrost']) {
+				this.add('-curestatus', pokemon, 'frz', '[from] move: ' + move);
+				pokemon.setStatus('');
+			}
+		},
+		onHit: function (target, source, move) {
+			if (move.thawsTarget || move.type === 'Fire' && move.category !== 'Status') {
+				target.cureStatus();
+			}
+		},
+	},
 solarsnow: {
 		name: 'SolarSnow',
 		id: 'solarsnow',
@@ -120,7 +192,7 @@ shadowdance: {
 					type = 'Normal';
 				}
 			}
-			pokemon.setType(type, true);
+			pokemon.setType(type, 'Fire');
 		},
 	},
 	onSourceModifyAccuracy: function (accuracy, pokemon) {
@@ -130,7 +202,7 @@ shadowdance: {
 	omneus: {
 		name: 'Omneus',
 		id: 'omneus',
-		num: 493,
+		num: 7500255,
 		onSwitchInPriority: 101,
 		onSwitchIn: function (pokemon) {
 			let type = 'Normal';
@@ -142,11 +214,11 @@ shadowdance: {
 					type = 'Normal';
 				}
 			}
-			pokemon.setType('Water', 'type');
+			pokemon.setType('Water', type);
 		},
 	},
 	onModifySpe: function (spe, pokemon) {
-			if (pokemon.item.name.includes("Plate") || pokemon.item.zMove) {
+			if (pokemon.getItem() && pokemon.getItem().onPlate) {
 				return this.chainModify(2);
 			}
 		},
