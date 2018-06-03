f09c2baa5f07c02a534c00d79e953f51b0b669c0
KirbyRider1337 <39121695+KirbyRider1337@users.noreply.github.com>
2018-06-02 21:44:06 -0400

    Update scripts.js (#849)
    * Update scripts.js
    
    * Update pokedex.js
    
    * Update pokedex.js

--- mods/fe/scripts.js
+++ mods/fe/scripts.js
@@ -4,6 +4,32 @@ const CHOOSABLE_TARGETS = new Set(['normal', 'any', 'adjacentAlly', 'adjacentAll
 
 /**@type {BattleScriptsData} */
 /*let BattleScripts = {
-
+	
+	canUltraBurst: function (pokemon) {
+	if (pokemon.getItem().id === 'ultranecroziumz'){
+			if (['Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane'].includes(pokemon.baseTemplate.species)) {
+				return "Necrozma-Ultra";
+			} else if (pokemon.baseTemplate.species === 'Necrynx') {
+				return "Necrynx-Ultra";
+			} else if (pokemon.baseTemplate.species === 'Necroqua') {
+				return "Necroqua-Ultra";
+			} else if (pokemon.baseTemplate.species === 'Necrozerain') {
+				return "Necrozerain-Ultra";
+			} else if (pokemon.baseTemplate.species === 'Lampara') {
+				return "Lampara-De-Lava";
+			} else if (pokemon.baseTemplate.species === 'Smotilizer') {
+				return "Smotilizer-Ultra";
+			} else if (pokemon.baseTemplate.species === 'Necropur') {
+				return "Necropur-Beautiful";
+			} else if (pokemon.baseTemplate.species === 'Chazma') {
+				return "Chazma-Hatched";
+			} else if (pokemon.baseTemplate.species === 'Necrotune') {
+				return "Necrotune-Ultra";
+			} else if (pokemon.baseTemplate.species === 'Nut') {
+				return "Ultra Burst Nut";
+			}
+		}
+		return null;
+	},
 };
 exports.BattleScripts = BattleScripts;*/
