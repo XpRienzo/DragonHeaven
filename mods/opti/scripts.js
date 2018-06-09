a1a5547bf30d0d4e582935537b5cf47358aafd18
charizard8888 <omkumr082001@outlook.com>
2018-06-09 12:09:33 +0530

    Update scripts.js

--- mods/opti/scripts.js
+++ mods/opti/scripts.js
@@ -21,5 +21,22 @@ this.modData('Learnsets', 'ambipom').learnset.flamewheel = ['6L1'];
 this.modData('Learnsets', 'ambipom').learnset.bulldoze = ['6L1'];
 this.modData('Learnsets', 'cinccino').learnset.rapidspin = ['6L1'];
 this.modData('Learnsets', 'cinccino').learnset.tremors = ['6L1'];
+	
+this.modData('Learnsets', 'azelf').learnset.swordsdance = ['7L1'];
+this.modData('Learnsets', 'azelf').learnset.focusblast = ['7L1'];
+this.modData('Learnsets', 'azelf').learnset.superpower = ['7L1'];
+this.modData('Learnsets', 'azelf').learnset.closecombat = ['7L1'];
+this.modData('Learnsets', 'azelf').learnset.machpunch = ['7L1'];
+this.modData('Learnsets', 'azelf').learnset.aurasphere = ['7L1'];
+this.modData('Learnsets', 'azelf').learnset.vacuumwave = ['7L1'];
+	
+this.modData('Learnsets', 'mesprit').learnset.recover = ['7L1'];
+this.modData('Learnsets', 'mesprit').learnset.moonblast = ['7L1'];
+this.modData('Learnsets', 'mesprit').learnset.nastyplot = ['7L1'];
+	
+this.modData('Learnsets', 'uxie').learnset.healingwish = ['7L1'];
+this.modData('Learnsets', 'uxie').learnset.slackoff = ['7L1'];
+this.modData('Learnsets', 'uxie').learnset.willowisp = ['7L1'];
+this.modData('Learnsets', 'uxie').learnset.passiveagressivity = ['7L1'];
 },
 };
