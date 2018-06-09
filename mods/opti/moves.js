270bad3c8b1cef828e152c9425f4c51e251a25f3
charizard8888 <omkumr082001@outlook.com>
2018-06-09 12:39:11 +0530

    Update moves.js

--- mods/opti/moves.js
+++ mods/opti/moves.js
@@ -103,4 +103,93 @@ exports.BattleMovedex = {
 		zMovePower: 100,
 		contestType: "Tough",
 	},
+	"flutterdust": {
+		accuracy: 100,
+		basePower: 50,
+		category: "Special",
+		desc: "Lowers the target's SpD, SpA and Spe by 1 stage.",
+		shortDesc: "Lowers the target's SpD, SpA and Spe by 1 stage",
+		id: "flutterdust",
+		isViable: true,
+		name: "Flutter Dust",
+		pp: 10,
+		priority: 0,
+		flags: {contact: 1, protect: 1, mirror: 1},
+		boosts: {
+				spa: -1,
+				spd: -1,
+				spe: -1,
+			},
+		target: "normal",
+		type: "Bug",
+		zMovePower: 100,
+		contestType: "Cute",
+	},
+	"beautydrain": {
+		accuracy: 100,
+		basePower: 80,
+		category: "Special",
+		desc: "Heals for 50% of the damage done.",
+		shortDesc: "User recovers 50% of the damage dealt.",
+		id: "beautydrain",
+		name: "Beauty Drain",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, heal: 1},
+		drain: [1, 2],
+		secondary: false,
+		target: "normal",
+		type: "Fairy",
+		zMovePower: 160,
+		contestType: "Cute",
+	},
+	"toxicturmoil": {
+		accuracy: 100,
+		basePower: 0,
+		category: "Status",
+		desc: "Removes 4 PP from last used move. If the opponent is poisoned, the PP reduction is doubled.",
+		shortDesc: "Removes 4 PP from last used move. If the opponent is poisoned, the PP reduction is doubled.",
+		id: "toxicturmoil",
+		name: "Toxic Turmoil",
+		pp: 20,
+		priority: 0,
+		flags: {protect: 1, reflectable: 1, mirror: 1, authentic: 1},
+		onHit: function (target) {
+			if (target.lastMove) {
+				let ppDeducted = target.deductPP(target.lastMove.id, 4);
+				if (target.status === 'psn' || target.status === 'tox') {
+					ppDeducted = target.deductPP(target.lastMove.id, 8);
+				}
+				if (ppDeducted) {
+					this.add("-activate", target, 'move: Spite', this.getMove(target.lastMove.id).name, ppDeducted);
+					return;
+				}
+			}
+			return false;
+		},
+		secondary: false,
+		target: "normal",
+		type: "Poison",
+		zMoveBoost: {spd: 1},
+		contestType: "Tough",
+	},
+	"stingstorm": {
+		accuracy: 100,
+		basePower: 200,
+		category: "Physical",
+		desc: "The user faints after using this move, even if this move fails for having no target. This move is prevented from executing if any active Pokemon has the Ability Damp.",
+		shortDesc: "Hits adjacent Pokemon. The user faints.",
+		id: "stingstorm",
+		isViable: true,
+		name: "Sting Storm",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		selfdestruct: "always",
+		secondary: false,
+		target: "allAdjacent",
+		type: "Normal",
+		zMovePower: 180,
+		contestType: "Beautiful",
+	},
 };
