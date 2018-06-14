768b51c9223a90a53977b38a638209f9d0450062
charizard8888 <omkumr082001@outlook.com>
2018-06-14 11:42:15 +0530

    Update pokedex.js

--- mods/tennysonmons/pokedex.js
+++ mods/tennysonmons/pokedex.js
@@ -12,7 +12,7 @@ abilities: {0: "Flame Body", 1: "Rock Head", H: "Adaptability"},
 },
 
 heatblastomnienhanced: {
-		num: 3,
+		num: 8001,
 		species: "Heat Blast-Omni Enhanced",
 		baseSpecies: "Heat Blast",
 		forme: "Omni Enhanced",
@@ -20,6 +20,24 @@ heatblastomnienhanced: {
 		types: ["Fire", "Electric"],
 		baseStats: {hp: 80, atk: 150, def: 100, spa: 150, spd: 100, spe: 180},
 		abilities: {0: "Speed Boost"},
+	},
+upgrade: {
+num: 8002,
+species: "Upgrade",
+types: ["Fire", "Flying"],
+baseStats: {hp: 80, atk: 120, def: 80, spa: 120, spd: 80, spe: 150},
+abilities: {0: "Flame Body", 1: "Rock Head", H: "Adaptability"},
+},
+
+upgradeomnienhanced: {
+		num: 8002,
+		species: "Upgrade-Omni Enhanced",
+		baseSpecies: "Upgrade",
+		forme: "Omni Enhanced",
+		formeLetter: "O",
+		types: ["Fire", "Electric"],
+		baseStats: {hp: 80, atk: 150, def: 100, spa: 150, spd: 100, spe: 180},
+		abilities: {0: "Imposter"},
 	},
   
  };
