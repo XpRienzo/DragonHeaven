f3f69199ce4a0d22a9326de6eadeb160464c4f73
charizard8888 <omkumr082001@outlook.com>
2018-06-14 11:25:19 +0530

    Create formats-data.js

---
+++ mods/tennysonmons/formats-data.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateFormatsData}} */
+let BattleFormatsData = {
+heatblast: {
+		tier: "OU",
+	},
+	heatblastomnienhanced: {
+		requiredItem: "Omnitrix",
+		tier: "OU",
+	},
+},
+};
+
+exports.BattleFormatsData = BattleFormatsData;
