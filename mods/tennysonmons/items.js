c5edc5cfbf6fd76681d1b74910b6ea2258f1bd4e
charizard8888 <omkumr082001@outlook.com>
2018-06-14 12:47:53 +0530

    Update items.js

--- mods/tennysonmons/items.js
+++ mods/tennysonmons/items.js
@@ -6,19 +6,6 @@ let BattleItems = {
 		id: "omnitrix",
 		name: "Omnitrix",
 		onTakeItem: false,
-		/*onStart: function(pokemon) {
-			this.add('-item', pokemon, 'Omnitrix');
-			if (pokemon.baseTemplate.baseSpecies === 'Heat Blast') {
-				this.add('-formechange', pokemon, 'Heat Blast-Omni Enhanced', '[msg]');
-				pokemon.formeChange("Heat Blast-Omni Enhanced");
-				pokemon.setAbility('speedboost');
-			}
-			else if (pokemon.baseTemplate.baseSpecies === 'Upgrade') {
-				this.add('-formechange', pokemon, 'Upgrade-Omni Enhanced', '[msg]');
-				pokemon.formeChange("Upgrade-Omni Enhanced");
-				pokemon.setAbility('intimidate');
-			}
-		},*/
 		desc: "If held by an Alien with an Omni-forme, it allows the Alien to Omni enhance in battle.",
 	},
   
