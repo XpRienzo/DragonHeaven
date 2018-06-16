452c40283884355dae8548e440995518b49f152d
Spandamn <spandan.punwatkar@gmail.com>
2018-06-16 13:18:28 +0530

    test

--- config/formats.js
+++ config/formats.js
@@ -3991,23 +3991,20 @@ exports.Formats = [
 			'Illegal', 'Unreleased', 'Shedinja', 'Infiltrator', 'Magic Guard', 'Misty Surge', 'Assault Vest', 'Choice Scarf', 'Explosion',
 			'Final Gambit', 'Healing Wish', 'Lunar Dance', 'Magic Room', 'Memento', 'Misty Terrain', 'Self-Destruct',
 		],
-		getEvoFamily: function (species) {
-			let template = Dex.getTemplate(species);
-			while (template.prevo) {
-				template = Dex.getTemplate(template.prevo);
-			}
-			return template.speciesid;
-		},
 		onValidateTeam: function (team, format) {
-			if (team.length !== 6) return [`Your team cannot have less than 6 Pok\u00e9mon.`];
-			// Family Clause
 			let problems = [];
-			for (let i = 0; i < team.length; i++) {
-				let set = team[i];
-				for (let j = i + 1; j < team.length; j++) {
-					if (format.getEvoFamily(set.species) === format.getEvoFamily(team[j].species)) {
-						problems.push(`You cannot have more than one Pokemon from their respective evolutionary line. (${set.name || set.species} and ${team[j].name || team[j].species} are from the same evolutionary line)`)
-					}
+			if (team.length !== 6) problems.push(`Your team cannot have less than 6 Pok\u00e9mon.`);
+			// Family Clause
+			let getEvoFamily = function (species) {
+				let template = Dex.getTemplate(species);
+				while (template.prevo) {
+					template = Dex.getTemplate(template.prevo);
+				}
+				return template.speciesid;
+			};
+			for (const [i, set] of team.entries()) {
+				if (format.getEvoFamily(set.species) === format.getEvoFamily(team[i + 1].species)) {
+					problems.push(`You cannot have more than one Pokemon from their respective evolutionary line. (${set.name || set.species} and ${team[i + 1].name || team[i + 1].species} are from the same evolutionary line)`);
 				}
 			}
 			return problems;
