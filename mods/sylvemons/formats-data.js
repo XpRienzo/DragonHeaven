af1ae5b66d35143a7b041d5e1a6491cf7f63c8b5
charizard8888 <omkumr082001@outlook.com>
2018-06-27 15:11:17 +0530

    Create formats-data.js

---
+++ mods/sylvemons/formats-data.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**@type {{[k: string]: TemplateFormatsData}} */
+let BattleFormatsData = {
+
+keldeo: {
+		randomBattleMoves: ["hydropump", "secretsword", "calmmind", "hiddenpowerflying", "hiddenpowerelectric", "substitute", "scald", "icywind"],
+		randomDoubleBattleMoves: ["hydropump", "secretsword", "protect", "hiddenpowerflying", "hiddenpowerelectric", "substitute", "surf", "icywind", "taunt"],
+		eventPokemon: [
+			{"generation": 5, "level": 15, "moves": ["aquajet", "leer", "doublekick", "bubblebeam"], "pokeball": "cherishball"},
+			{"generation": 5, "level": 50, "moves": ["sacredsword", "hydropump", "aquajet", "swordsdance"], "pokeball": "cherishball"},
+			{"generation": 6, "level": 15, "moves": ["aquajet", "leer", "doublekick", "hydropump"], "pokeball": "cherishball"},
+			{"generation": 6, "level": 100, "moves": ["aquajet", "leer", "doublekick", "bubblebeam"], "pokeball": "cherishball"},
+		],
+		//eventOnly: true,
+		tier: "OU",
+		doublesTier: "DUU",
+	},
+	keldeoresolute: {
+		//eventOnly: true,
+		requiredMove: "Secret Sword",
+	},
+  
+ };
+
+exports.BattleFormatsData = BattleFormatsData;
